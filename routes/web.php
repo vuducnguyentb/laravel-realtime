6d38d5860da4e7be13e16c6f9b951d2a5b35591f
vuducnguyentb <vuducnguyentb@gmail.com>
2023-09-07 00:00:09 +0700

    config and create event

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/playgroud', function () {
+    echo 'vô';
+    event(new \App\Events\PlaygroudEvent());
+    return null;
+});
